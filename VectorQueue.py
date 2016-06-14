6c6562e829edd7be394835115390e22b256641c3
haoliangyu <njkon@foxmail.com>
2016-06-13 20:08:07 -0400

    add queue version merge sort

--- VectorQueue.py
+++ VectorQueue.py
@@ -14,3 +14,26 @@ class VectorQueue(Vector):
 
     def front(self):
         return None if self.empty() else self.data[0]
+
+    def __merge(self, lo, mi, hi):
+        # prepare left queue
+        left_queue = VectorQueue()
+        for i in range(lo, mi):
+            left_queue.enqueue(self.data[i])
+
+        # prepare right queue
+        right_queue = VectorQueue()
+        for i in range(mi, hi):
+            right_queue.enqueue(self.data[i])
+
+        # result queue
+        merged = VectorQueue()
+
+        while not left_queue.empty() or not right_queue.empty():
+            if not left_queue.empty() and (right_queue.empty() or left_queue.front() <= right_queue.front()):
+                merged.enqueue(left_queue.dequque())
+
+            if not right_queue.empty() and (left_queue.empty() or right_queue.front() < left_queue.front()):
+                merged.enqueue(right_queue.dequque)
+
+        self.data = merged.data
