aea283fd945e941af44bf7855986ffac9d66932a
haoliangyu <njkon@foxmail.com>
2016-09-18 13:10:29 -0400

    improve Heap performance

--- Heap.py
+++ Heap.py
@@ -1,8 +1,12 @@
 class Heap(object):
 
     def __init__(self, data=[]):
-        self.data = data
-        self.__size = len(data)
+        if len(data) == 0:
+            self.data = [None] * 100
+        else:
+            self.data = data
+
+        self.__size = sum([1 if item is not None else 0 for item in self.data])
         self.__heapify()
 
     def size(self):
@@ -16,7 +20,8 @@ class Heap(object):
 
     def delete_max(self):
         max_data = self.data[0]
-        self.__swap(0, self.size() - 1)
+        self.__swap(0, self.__size - 1)
+        self.data[self.__size - 1] = None
         self.__size -= 1
 
         self.__percolate_down(0)
@@ -24,10 +29,13 @@ class Heap(object):
         return max_data
 
     def insert(self, number):
+        if self.__size == len(self.data):
+            self.__expand()
+
         self.__size += 1
         self.data[self.__size - 1] = number
 
-        return self.__percolate_up(self.size() - 1)
+        return self.__percolate_up(self.__size - 1)
 
     @staticmethod
     def heap_sort(data):
@@ -41,29 +49,61 @@ class Heap(object):
         return heap.data
 
     def __percolate_down(self, i):
+        initial_value = self.data[i]
         current_index = i
         potential_parent = self.__proper_parent(current_index)
 
         while self.data[potential_parent] > self.data[current_index]:
-            self.__swap(potential_parent, current_index)
+            self.data[current_index] = self.data[potential_parent]
             current_index = potential_parent
             potential_parent = self.__proper_parent(current_index)
+
+        self.data[current_index] = initial_value
 
         return current_index
 
     def __percolate_up(self, i):
+        if not self.__has_parent(i):
+            return 0
+
+        initial_value = self.data[i]
+        parent_indexes = []
+
+        h = 1
         current_index = i
-
         while self.__has_parent(current_index):
-            parent_index = self.__parent(current_index)
+            current_index = ((i + 1) >> h) - 1
+            parent_indexes.append(current_index)
+            h += 1
 
-            if self.data[current_index] <= self.data[parent_index]:
-                return current_index
+        lo = 0
+        hi = len(parent_indexes) - 1
+        while lo + 1 < hi:
+            mi = (lo + hi) / 2
+
+            if self.data[parent_indexes[mi]] <= self.data[i]:
+                lo = mi
             else:
-                self.__swap(current_index, parent_index)
-                current_index = parent_index
+                hi = mi
 
-        return 0
+
+        parent_indexes.insert(0, i)
+        lo = lo + 1
+        index = 0
+        while index < lo:
+            self.data[parent_indexes[index]] = self.data[parent_indexes[index + 1]]
+            index += 1
+
+        self.data[parent_indexes[lo]] = initial_value
+
+        return parent_indexes[lo]
+
+    def __expand(self):
+        new_data = [None] * (self.__size * 2)
+        for i in range(self.__size):
+            new_data[i] = self.data[i]
+
+        self.data = new_data
 
     def __heapify(self):
         i = self.__last_internal()
